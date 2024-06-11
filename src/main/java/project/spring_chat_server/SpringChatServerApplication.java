ce602d8a05a9a2802eddfd1eb99f49365cb75bfb
kimdohyung <pdohyung@gmail.com>
2024-06-11 16:16:51 +0900

    Initial commit

---
+++ src/main/java/project/spring_chat_server/SpringChatServerApplication.java
@@ -0,0 +1,13 @@
+package project.spring_chat_server;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringChatServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringChatServerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/project/spring_chat_server/SpringChatServerApplicationTests.java
@@ -0,0 +1,13 @@
+package project.spring_chat_server;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringChatServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
